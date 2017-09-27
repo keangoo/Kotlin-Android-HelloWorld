88d5f89fe856ff80e3fb84397ed59f8e5e259657
Kean <keangoo@gmail.com>
2017-09-27 15:18:57 +0700

    Init Project

---
+++ app/src/main/java/com/example/fuck/fuckapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.fuck.fuckapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Toan on 27/09/17.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
